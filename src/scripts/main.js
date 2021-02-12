9328dbfa633f58d7c6e06fc9f8cebe9e82755c44
SaAlgervik <70169634+SaAlgervik@users.noreply.github.com>
2021-02-12 13:38:27 +0100

    Adding login
    Just findig my way around how things work and moves with rem attribute

---
+++ src/scripts/main.js
@@ -0,0 +1,10 @@
+
+function saveData(){
+    var userName = document.getElementById("userName").value;
+    console.log(userName)
+    var password = document.getElementById("password").value;
+    console.log(password)
+
+
+
+}
