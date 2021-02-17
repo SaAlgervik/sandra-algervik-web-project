ddd8c6becc51b08643b41a8690c9b91e1518d281
SaAlgervik <70169634+SaAlgervik@users.noreply.github.com>
2021-02-17 14:10:09 +0100

    making change to image onclick
    Making the image change depending on buttonclick

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,29 +1,48 @@
+var homePageImage ="(https://images.unsplash.com/photo-1503924087716-07cbd5f49b21?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=700&q=8)";
+var aboutPageImage = "https://images.pexels.com/photos/789812/pexels-photo-789812.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
 
-var homePageContent = document.getElementById(content-home-page);
-var hello = "jag gillar glass";
+function checkSelected(selected){
+
+    if(selected === "HOME"){
+      
+        changeImage1('main_imageSection')
 
 
-function getHomePageText(){
-   console.log("HomePage") 
-//document.getElementById("content").textContent //= document.getElementById("content-home-page");
-  
-}
+    }
+    if(selected === "ABOUT"){
+         changeImage('main_imageSection')
 
-function hideContent(contentToHide){
-    
-    var x = document.getElementById(contentToHide);
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
     }
 }
 
+
+
+function changeImage(image){
+    
+   document.getElementsByClassName(image)[0].style.backgroundImage  = "url('https://images.pexels.com/photos/789812/pexels-photo-789812.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')";
+    
+}
+function changeImage1(image){
+    
+    document.getElementsByClassName(image)[0].style.backgroundImage  = "url('https://images.unsplash.com/photo-1503924087716-07cbd5f49b21?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=700&q=8')";
+     
+ }
+
+
+function hideContent(contentToHide,contentToShow){
+    
+    var x = document.getElementById(contentToHide);
+    var y = document.getElementById
+   (contentToShow);
+    if (x.style.display === y.style.display) {
+      x.style.display = "none";
+      y.style.display = "block";
+    } 
+}
+
 function aboutButtonClick(){
- hideContent("content-home-page");
- hideContent("home-page-picture");
- console.log("hej")
- //hideContent("home-page-pictrure");
+// hideContent("content-home-page", "about-page-content");
+ hideContent("home-page-picture","about-page-picture");
 }
 
 
