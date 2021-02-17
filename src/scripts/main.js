010e434ca6f21737ad7d1ec5ddfaab411a23db32
SaAlgervik <70169634+SaAlgervik@users.noreply.github.com>
2021-02-17 10:04:37 +0100

    Change to flexbox
    I changed the layout to flexboxes instead so it will be easier to scale. Starting with functions for te buttons

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,29 @@
 
-function saveData(){
-    var userName = document.getElementById("userName").value;
-    console.log(userName)
-    var password = document.getElementById("password").value;
-    console.log(password)
+var homePageContent = document.getElementById(content-home-page);
+var hello = "jag gillar glass";
 
+
+function getHomePageText(){
+   console.log("HomePage") 
+//document.getElementById("content").textContent //= document.getElementById("content-home-page");
+  
+}
+
+function hideContent(contentToHide){
+    
+    var x = document.getElementById(contentToHide);
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function aboutButtonClick(){
+ hideContent("content-home-page");
+ hideContent("home-page-picture");
+ console.log("hej")
+ //hideContent("home-page-pictrure");
 }
 
 
