6d6ec11ef2a4b69c86966ec63fc5a06825b1eae0
SaAlgervik <70169634+SaAlgervik@users.noreply.github.com>
2021-02-25 14:14:34 +0100

    final changes of method
    Making the method work on all sites and took away the things i don't need

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,102 +2,78 @@ const homePageImage = "https://images.unsplash.com/photo-1503924087716-07cbd5f49
 const aboutPageImage = "https://images.pexels.com/photos/789812/pexels-photo-789812.jpeg?auto=compress&c" +"s=tinysrgb&dpr=1&w=500";
 const APIpageImage ="https://images.pexels.com/photos/1055379/pexels-photo-1055379.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 
-
 function checkSelected(selected) {
 
     if (selected === "HOME") {
         clearText();
         changeMainImage(homePageImage);
-        changeMainText('about_text','home_text');
-        document.getElementsByClassName("textSection_search")[0].style.display ="none";
-
-
-
-
+        changeMainText('about_text',"plant_name",'home_text');
     }
     if (selected === "ABOUT") {
         clearText();
         changeMainImage(aboutPageImage);
-        changeMainText('home_text', 'about_text');
-        document.getElementsByClassName("textSection_search")[0].style.display ="none";
-
+        changeMainText('home_text',"plant_name", 'about_text');
 
     }
     if (selected === "API") {
         changeMainImage(APIpageImage);
-        displayAPIsearch();
-
+        changeMainText('home_text','about_text','plant_name' );
+        displayAPISearch();
 
          //detta va längen för log in sidan som skulle vart med ifrån början//  
         //window.location.href = 'https://saalgervik.github.io/sandra-algervik-web-project/account.html'
-
     }
 }
 
 
-function changeMainText(hide, show){
-    document.getElementById(hide).style.display ="none";
+function changeMainText(firstToHide,secondToHide, show){
+    document.getElementById(firstToHide).style.display ="none";
+    document.getElementById(secondToHide).style.display ="none";
     document.getElementById(show).style.display ="block";
 }
 
-function displayAPIsearch(){
-    document.getElementById('about_text').style.display ="none";
-    document.getElementById('home_text').style.display ="none";
+function displayAPISearch(){
     document.getElementsByClassName("textSection_search")[0].style.display ="block";
-
-
 
 }
 
-
 function start() {
-
     document.getElementById("about_text").style.display ="none";
+    document.getElementById("plant_name").style.display ="none";
     document.getElementsByClassName("textSection_search")[0].style.display ="none";
 }
 
-function hideContent(contentToHide, contentToShow) {
-
-    var x = document.getElementById(contentToHide);
-    var y = document.getElementById(contentToShow);
-    if (x.style.display === y.style.display) {
-        x.style.display = "none";
-        y.style.display = "block";
-    }
-}
-
+// Submits text if Enter is pressed
 function keyEnter(event){
     if(event.keyCode==13) {
         submitText();
     }
 }
-
+// sends text to axios to make a fetch from the API 
 function submitText() {
     var inputText = document.getElementById("common_name").value;
     document.getElementById("common_name").value = null;
     clearText()
     getCommonName(inputText);
 }
-//
+
 function changeMainImage(imageSource) {
     var mainImageRef = document.getElementsByClassName('main_imageSection')[0]
     mainImageRef.setAttribute("style", "background-image: url(" + imageSource + ");");
 }
-
+// removes the earlier search from the API if one have been done
 function clearText(){
     if(document.getElementsByClassName('common_name').length !=0){
         let  i = 0;
-
          do{
-             const list = document.getElementsByClassName('common_name')[i]
-             list.innerHTML = "";
+             document.getElementsByClassName('common_name')[i].innerHTML ="";
              
             i++
          }while(i < document.getElementsByClassName('common_name').length)
     }
 }
 
-// hämtar data från API via axios
+// Gets data from API by axios
 function getCommonName(inputText) {
     
     axios
@@ -127,6 +103,7 @@ function getCommonName(inputText) {
 
                 i++
             } while (i < data.length);
+          
            
         });
 
